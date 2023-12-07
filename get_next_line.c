5eeca87a23fc1fac60a8db01340a3c4978f6a5d2
Asmae Tahtouh <atahtouh@e2r12p5.1337.ma>
2023-12-07 12:19:49 +0100

    get_next

---
+++ get_next_line.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahtouh <atahtouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:22:33 by atahtouh          #+#    #+#             */
+/*   Updated: 2023/12/07 12:09:55 by atahtouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int ft_check_line(char *s)
+{
+	int i;
+	
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_read(int fd, char *s)
+{
+	int	i ;
+	char *ptr;
+	int bytes_read;
+	int nb;
+
+	i = 0;
+	ptr = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if(!ptr)
+		return (NULL);
+	bytes_read = 1;
+	while(bytes_read > 0)
+	{
+		bytes_read = read(fd,ptr,BUFFER_SIZE);
+		if(bytes_read == -1)
+			break;
+		ptr[bytes_read] = '\0';
+		// if (!s)
+		// 	s = ft_strjoin(NULL,ptr);
+		//else
+			s = ft_strjoin(s,ptr);
+		nb = ft_check_line(s);
+		if(nb > 0)
+			break;
+	}
+	return (s);
+}
+char *ft_extrat_line(char *s)
+{
+	int i;
+	char *ptr;
+	
+	i = 0;
+	if(!s[0])
+		return (NULL);
+	while(s[i] && s[i] != '\n')
+		i++;
+	ptr = ft_substr(s, 0, i + 1);
+	return (ptr);
+}
+
+char *ft_rest(char *s)
+{
+	int i;
+	char *ptr;
+
+	i = 0;
+	while(s[i] && s[i] != '\n')
+		i++;
+	if(!s[i])
+	{
+		free(s);
+		return(NULL);
+	}
+	i++;
+	ptr = ft_substr(s, i, ft_strlen(s) - i);
+	free(s);
+	return (ptr);
+}
+
+
+char *get_next_line(int fd)
+{
+	static char *p1;
+	char *s;
+	char *ligne; 
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	s = ft_read(fd, p1);
+	if(!s)
+		return (NULL);
+	ligne = ft_extrat_line(s);
+	p1 = ft_rest(s);
+	return (ligne);
+}
+
+
+
---
+++ get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahtouh <atahtouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 19:59:42 by atahtouh          #+#    #+#             */
+/*   Updated: 2023/12/07 11:30:42 by atahtouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	int i;
+	int j;
+	char *ptr;
+	int all;
+	
+	i = 0;
+	j = 0;
+	all = ft_strlen(s1) + ft_strlen(s2);
+	ptr = (char *)malloc((all + 1) * sizeof(char));
+	if(!ptr)
+		return (NULL);
+	while(s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while(s2[j])
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr [i] = '\0';
+	return (ptr);
+}
+
+char *ft_strchar(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+	{
+		if(s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char *ft_substr(char *s, int start, int size)
+{
+	int i;
+	int j;
+	char *ptr;
+
+	i = 0;
+	j = 0;
+	ptr = (char *)malloc((size + 1) * sizeof(char));
+	if(!ptr)
+		return(NULL);
+	while(s[i] && i < start)
+		i++;
+	while(s[i] && j < size)
+	{
+		ptr[j] = s[i];
+		i++;
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
